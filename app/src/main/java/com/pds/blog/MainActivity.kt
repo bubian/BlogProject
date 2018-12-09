b73bf830a1d36bbf78cf6d3242dd322ecace504e
pengdaosong <pengdaosong@medlinker.com>
2018-12-09 15:09:03 +0800

    Initial commit

---
+++ app/src/main/java/com/pds/blog/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pds.blog
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
