f3e50e6bbb1e591fd1068293bbeff88a77edbcc1
pengdaosong <pengdaosong@medlinker.com>
2019-05-15 17:31:09 +0800

    删除无用的文件

---
+++ socket/src/main/java/blog/pds/com/socket/core/client/SocketManager.kt
@@ -0,0 +1,17 @@
+package blog.pds.com.socket.core.client
+
+/**
+ * @author: pengdaosong
+ * CreateTime:  2019/5/14 2:41 PM
+ * Email：pengdaosong@medlinker.com
+ * Description:
+ */
+
+class SocketManager{
+    companion object {
+        fun reconnect(){
+
+        }
+    }
+
+}
