f330253eec1897d7e6ec5adffe5480027faffab1
pengdaosong <pengdaosong@medlinker.com>
2020-01-02 14:48:43 +0800

    任务预加载

---
+++ lib_preloader/src/main/java/com/pds/preloader/StateDone.java
@@ -0,0 +1,37 @@
+package com.pds.preloader;
+
+
+import com.pds.preloader.interfaces.DataListener;
+
+/**
+ * pre-load finished
+ * Preloader.refresh() and PreLoader.listen(DataListener listener) are valid
+ */
+class StateDone extends StateBase {
+    StateDone(Worker<?> worker) {
+        super(worker);
+    }
+
+    @Override
+    public boolean refresh() {
+        super.refresh();
+        return worker.doStartLoadWork() && worker.listenData();
+    }
+
+    @Override
+    public boolean listenData() {
+        super.listenData();
+        return worker.doSendLoadedDataToListenerWork();
+    }
+
+    @Override
+    public boolean listenData(DataListener listener) {
+        super.listenData(listener);
+        return worker.doSendLoadedDataToListenerWork(listener);
+    }
+
+    @Override
+    public String name() {
+        return "StateDone";
+    }
+}
