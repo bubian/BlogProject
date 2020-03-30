a05538ba184447893467802efc99ac3f57d5cf64
pengdaosong <pengdaosong@medlinker.com>
2020-03-30 11:24:07 +0800

    优化代码

--- app/src/main/java/com/pds/blog/test/RxJavaTest.java
+++ app/src/main/java/com/pds/blog/test/RxJavaTest.java
@@ -1,26 +1,19 @@
-package com.pds.blog;
-
-import android.util.Log;
+package com.pds.blog.test;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.ObservableEmitter;
 import io.reactivex.rxjava3.core.ObservableOnSubscribe;
 import io.reactivex.rxjava3.core.ObservableSource;
 import io.reactivex.rxjava3.core.Observer;
-import io.reactivex.rxjava3.core.Scheduler;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.functions.Action;
 import io.reactivex.rxjava3.functions.BiFunction;
-import io.reactivex.rxjava3.functions.Consumer;
 import io.reactivex.rxjava3.functions.Function;
 import io.reactivex.rxjava3.functions.Supplier;
-import io.reactivex.rxjava3.internal.operators.observable.ObservableFlatMap;
-import io.reactivex.rxjava3.schedulers.Schedulers;
 
 /**
  * @author: pengdaosong
