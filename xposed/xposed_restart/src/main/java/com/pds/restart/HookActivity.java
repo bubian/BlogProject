9bf5afc64b4d4f703fb7b333179715f86fed6343
pengdaosong <pengdaosong@medlinker.com>
2020-04-12 21:47:12 +0800

    xposed免重启设置

---
+++ xposed/xposed_restart/src/main/java/com/pds/restart/HookActivity.java
@@ -0,0 +1,21 @@
+package com.pds.restart;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author: pengdaosong
+ * CreateTime:  2020-04-12 21:23
+ * Email：pengdaosong@medlinker.com
+ * Description:
+ */
+public class HookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ xposed/xposed_restart/src/main/java/com/pds/restart/hook/RealHook.java
@@ -0,0 +1,10 @@
+package com.pds.restart.hook;
+
+/**
+ * @author: pengdaosong
+ * CreateTime:  2020-04-12 21:38
+ * Email：pengdaosong@medlinker.com
+ * Description:
+ */
+public class RealHook {
+}
