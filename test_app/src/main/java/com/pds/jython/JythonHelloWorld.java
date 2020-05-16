30c3ba6dbfcbdd8ff52129b4f3ef77ed987eb21b
pengdaosong <pengdaosong@medlinker.com>
2020-05-16 11:18:54 +0800

    jython:java执行python程序

---
+++ test_app/src/main/java/com/pds/jython/JythonHelloWorld.java
@@ -0,0 +1,19 @@
+package com.pds.jython;
+
+import org.python.util.PythonInterpreter;
+
+/**
+ * @author: pengdaosong
+ * CreateTime:  2020-05-16 11:13
+ * Email：pengdaosong@medlinker.com
+ * Description:
+ */
+public class JythonHelloWorld {
+
+    public static void main(String[] args){
+        try (PythonInterpreter pythonInterpreter = new PythonInterpreter()){
+            pythonInterpreter.exec("print('Hello Python World!')");
+
+        }
+    }
+}
