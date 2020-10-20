cfbd19e891c4ee0c52c4c0e637d031f2baa5c29a
pengdaosong <pengdaosong@medlinker.com>
2020-10-20 17:17:18 +0800

    添加pdf打开

---
+++ module/SampleModule/src/main/java/com/pds/sample/module/pdf/PDFWebLoadActivity.java
@@ -0,0 +1,97 @@
+package com.pds.sample.module.pdf;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.Nullable;
+
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.pds.base.act.BaseActivity;
+import com.pds.sample.R;
+import com.pds.sample.router.ARouterPath;
+
+import java.io.File;
+
+/**
+ * @author: pengdaosong
+ * @CreateTime: 2020/10/20 2:46 PM
+ * @Email: pengdaosong@medlinker.com
+ * @Description:
+ */
+@Route(path = ARouterPath.WEB_PDF)
+public class PDFWebLoadActivity extends BaseActivity {
+
+    private static final String PDF_URL = "https://pub-med-casem.medlinker.com/guanxin_paitent_test.pdf";
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_pdf);
+        mWebView = findViewById(R.id.web_view);
+        initSetting(mWebView);
+        mWebView.loadUrl(buildUrl(PDF_URL));
+    }
+
+    protected void initSetting(WebView webView) {
+        WebSettings settings = webView.getSettings();
+
+        settings.setAppCacheEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setLayoutAlgorithm(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT
+                ? WebSettings.LayoutAlgorithm.NORMAL : WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        settings.setSupportZoom(true);
+        settings.setBuiltInZoomControls(true);
+        settings.setUseWideViewPort(true);
+        settings.setSupportMultipleWindows(false);
+        settings.setLoadWithOverviewMode(true);
+        settings.setDatabaseEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setJavaScriptEnabled(true);
+        settings.setGeolocationEnabled(true);
+        settings.setAppCacheMaxSize(Long.MAX_VALUE);
+        settings.setAppCachePath(new File(webView.getContext().getCacheDir(), "wb_appcache").getAbsolutePath());
+        settings.setDatabasePath(new File(webView.getContext().getCacheDir(), "wb_databases").getAbsolutePath());
+        settings.setGeolocationDatabasePath(new File(webView.getContext().getCacheDir(), "wb_geolocation").getAbsolutePath());
+        settings.setPluginState(WebSettings.PluginState.ON_DEMAND);
+        settings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        // settings.setUserAgentString("");
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            //方便webview在choreme调试
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+
+        //Http和Https混合问题
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            settings.setMixedContentMode(android.webkit.WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+    }
+
+    /**
+     * 通过网页导入PDF文件
+     * <p>
+     * 参考：https://github.com/mozilla/pdf.js
+     *
+     * @param view
+     */
+    public void doWebLoadPdf(View view) {
+
+    }
+
+    private String buildUrl(String pdfUrl) {
+        return getPdfLink(true) + "?file=" + pdfUrl;
+    }
+
+    private String getPdfLink(boolean isOld) {
+        return isOld ? "https://mozilla.github.io/pdf.js/es5/web/viewer.html" : "https://mozilla.github.io/pdf.js/web/viewer.html";
+    }
+}
