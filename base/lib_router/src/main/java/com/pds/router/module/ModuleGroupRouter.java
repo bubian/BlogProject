8a249c4b25ef7651c55272e4d3a2a33cb00aa460
pengdaosong <pengdaosong@medlinker.com>
2020-11-15 23:22:35 +0800

    优化代码

--- base/lib_router/src/main/java/com/pds/router/module/ModuleGroupRouter.java
+++ base/lib_router/src/main/java/com/pds/router/module/ModuleGroupRouter.java
@@ -5,20 +5,24 @@ package com.pds.router.module;
  * @CreateTime: 2020/10/24 10:09 AM
  * @Email: pengdaosong@medlinker.com
  * @Description:
+ *
+ * 分组名称必须和module所在的文件夹名称相同，比如business目录下的module使用的路由，路由组名要用business，不然跳转失败，找不到对应的组。
+ * 如果在相同的目录下，可以不用。
  */
 public class ModuleGroupRouter {
-    private static final String LIB = "/module";
-    public static final String PDF = LIB +"/pdf";
+    private static final String LIB = "/base";
     public static final String HYBRID = LIB + "/hybrid";
-    public static final String RN_ACTIVITY = LIB + "/rnActivity";
-    public static final String RN_FRAGMENT = LIB + "/rnFragment";
 
-    public static final String FLUTTER_ACTIVITY = LIB + "/flutterActivity";
-    public static final String FLUTTER_FRAGMENT = LIB + "/flutterFragment";
+    private static final String REACT = "/react";
+    public static final String RN_ACTIVITY = REACT + "/rnActivity";
+    public static final String RN_FRAGMENT = REACT + "/rnFragment";
 
-    public static final String ADDRESS_FIND = LIB + "/addressFind";
+    private static final String FLUTTER = "/flutter";
+    public static final String FLUTTER_ACTIVITY = FLUTTER + "/flutterActivity";
+    public static final String FLUTTER_FRAGMENT = FLUTTER + "/flutterFragment";
 
-    public static final String LINK_HYBRID = "/link/hybrid";
+    private static final String BUSINESS = "/business";
+    public static final String ADDRESS_FIND = BUSINESS + "/addressFind";
 
     private static final String LIB_STUDY = "/study";
     public static final String UI_STUDY = LIB_STUDY + "/uiStudy";
@@ -26,4 +30,7 @@ public class ModuleGroupRouter {
 
     private static final String LIB_PLUGIN = "/plugin";
     public static final String PLUGIN_PHONE_PROXY = LIB_PLUGIN + "/proxy";
+
+    // 其它
+    public static final String LINK_HYBRID = "/link/hybrid";
 }
