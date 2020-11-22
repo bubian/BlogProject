19368a2a921ff9e73900a5e486bd7461ea0a1d8a
pengdaosong <pengdaosong@medlinker.com>
2020-11-22 17:36:32 +0800

    优化

--- base/lib_router/src/main/java/com/pds/router/module/MainGroupRouter.java
+++ base/lib_router/src/main/java/com/pds/router/module/MainGroupRouter.java
@@ -6,11 +6,13 @@ package com.pds.router.module;
  * @Email: pengdaosong@medlinker.com
  * @Description:
  *
- *  * 分组名重要注意事项：根据平时使用过程发现：相同"组"里定义的路由不能使用在不同module中，不然先定义的"路由"跳转失败，最后定义的可以正常跳转。
- *  * 比如下面定义了分组LIB = "/base"和路由HYBRID = LIB + "/hybrid"，而路由在web module中使用，如果这时候路由XXXX = LIB + "/xxx"，
- *  * 在除web module的其它module中使用，那么会导致跳转web module路由失败，而跳转定义路由所在的module跳转成功。
+ * 分组名重要注意事项：根据平时使用过程发现：相同"组"里定义的路由不能使用在不同module中，不然先定义的"路由"跳转失败，最后定义的可以正常跳转。
+ * 比如下面定义了分组LIB = "/base"和路由HYBRID = LIB + "/hybrid"，而路由在web module中使用，如果这时候路由XXXX = LIB + "/xxx"，
+ * 在除web module的其它module中使用，那么会导致跳转web module路由失败，而跳转定义路由所在的module跳转成功。
+ *
+ * 建议：所以定义路由时，组名最好以module名开头，这样避免上面的问题，而且分组可以一定程度上优化内存
  */
 public class MainGroupRouter {
-    private static final String MAIN = "/main";
+    private static final String MAIN = "/mainModule";
     public static final String HOME = MAIN + "/home";
 }
--- base/lib_router/src/main/java/com/pds/router/module/ModuleGroupRouter.java
+++ base/lib_router/src/main/java/com/pds/router/module/ModuleGroupRouter.java
@@ -5,39 +5,41 @@ package com.pds.router.module;
  * @CreateTime: 2020/10/24 10:09 AM
  * @Email: pengdaosong@medlinker.com
  * @Description:
- *
- * 分组名重要注意事项：根据平时使用过程发现：相同"组"里定义的路由不能使用在不同module中，不然先定义的"路由"跳转失败，最后定义的可以正常跳转。
+ * <p>
+ * 注意：分组名重要注意事项：根据平时使用过程发现：相同"组"里定义的路由不能使用在不同module中，不然先定义的"路由"跳转失败，最后定义的可以正常跳转。
  * 比如下面定义了分组LIB = "/base"和路由HYBRID = LIB + "/hybrid"，而路由在web module中使用，如果这时候路由XXXX = LIB + "/xxx"，
  * 在除web module的其它module中使用，那么会导致跳转web module路由失败，而跳转定义路由所在的module跳转成功。
+ * <p>
+ * 建议：所以定义路由时，组名最好以module名开头，这样避免上面的问题，而且分组可以一定程度上优化内存。
  */
 public class ModuleGroupRouter {
-    private static final String LIB = "/base";
+    private static final String LIB = "/web";
     public static final String HYBRID = LIB + "/hybrid";
 
-    private static final String REACT = "/react";
+    private static final String REACT = "/RnModule";
     public static final String RN_ACTIVITY = REACT + "/rnActivity";
     public static final String RN_FRAGMENT = REACT + "/rnFragment";
 
-    private static final String FLUTTER = "/flutter";
+    private static final String FLUTTER = "/FlutterModule";
     public static final String FLUTTER_ACTIVITY = FLUTTER + "/flutterActivity";
     public static final String FLUTTER_FRAGMENT = FLUTTER + "/flutterFragment";
 
-    private static final String BUSINESS = "/business";
-    public static final String ADDRESS_FIND = BUSINESS + "/addressFind";
+    private static final String AL = "/AddressLookup";
+    public static final String ADDRESS_FIND = AL + "/addressFind";
 
-    private static final String LIB_STUDY = "/study";
+    private static final String LIB_STUDY = "/kotlin_study";
     public static final String UI_STUDY = LIB_STUDY + "/uiStudy";
     public static final String UI_AUDIO_RECORD = LIB_STUDY + "/audioRecord";
 
-    private static final String LIB_S_PLUGIN = "/splugin";
+    private static final String LIB_S_PLUGIN = "/lib_splugin";
     public static final String PLUGIN_PHONE_PROXY = LIB_S_PLUGIN + "/proxy";
     public static final String PLUGIN_ACTIVITY_REPLACE = LIB_S_PLUGIN + "/activityReplace";
 
-    private static final String LIB_PLUGIN = "/plugin";
+    private static final String LIB_PLUGIN = "/lib_plugin";
     public static final String PLUGIN_COMMERCIAL_PLUGIN = LIB_PLUGIN + "/commercial";
 
-    private static final String LIB_GOOGLE = "/google";
-    public static final String GOOGLE_VIEW_WIDGET = LIB_GOOGLE + "/viewWidget";
+    private static final String LIB_ML = "/MotionLayout";
+    public static final String GOOGLE_VIEW_WIDGET = LIB_ML + "/viewWidget";
 
     // 其它
     public static final String LINK_HYBRID = "/link/hybrid";
